c662e78e396c81dd939bb16ad952b6a361c6201b
sergiy-ta <serhiy819@gmail.com>
2021-02-28 20:05:57 +0200

    added method get list to api of department

--- server/src/api/v1/department.api.ts
+++ server/src/api/v1/department.api.ts
@@ -4,6 +4,11 @@ import DepartmentService from '../../service/department/department.service';
 
 const router: express.Router = express.Router();
 
+router.get('/department', async (req: express.Request, res: express.Response) => {
+    const departmentService: DepartmentService = new DepartmentService();
+    APIService.processingOnAPIOfDataModels({ req, res, method: departmentService.getList(), dataError: null });
+});
+
 router.get('/department/:id', async (req: express.Request, res: express.Response) => {
     const departmentService: DepartmentService = new DepartmentService();
     APIService.processingOnAPIOfDataModels({ req, res, method: departmentService.get({ dpID: +req.params.id }), dataError: null });
