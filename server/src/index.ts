d95281ac62e90cb8790b65930a30f18cb28a4271
sergiy-ta <serhiy819@gmail.com>
2021-02-28 16:56:40 +0200

    added api for method post department

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
+import cors from 'cors';
+
+import api from './api/api';
 
 dotenv.config();
 
 const app: express.Application = express();
 const port: string | number = process.env.PORT ?? 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/api', api);
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).json({
