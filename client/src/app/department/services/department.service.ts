e45959f41999b2634d343b50315a4b43f37d0f6f
sergiy-ta <serhiy819@gmail.com>
2021-02-28 17:09:50 +0200

    added host

--- client/src/app/department/services/department.service.ts
+++ client/src/app/department/services/department.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,6 @@ export class DepartmentService {
   ) { }
 
   save(info: { name: string }): Observable<{ data: { name: string } }> {
-    console.log(info);
-    return this.http.post<{ data: { name: string } }>('/api/v1/department', info);
+    return this.http.post<{ data: { name: string } }>(environment.urlForServer + '/api/v1/department', info);
   }
 }
