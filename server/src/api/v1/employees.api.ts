6eed4678ed351ef638cddfd6515b094e3b8c4385
sergiy-ta <serhiy819@gmail.com>
2021-03-01 23:10:38 +0200

    added method get for api employees

--- server/src/api/v1/employees.api.ts
+++ server/src/api/v1/employees.api.ts
@@ -4,6 +4,11 @@ import EmployeesService from '../../service/employees/employees.service';
 
 const router: express.Router = express.Router();
 
+router.get('/employees/:id', async (req: express.Request, res: express.Response) => {
+    const employeesService: EmployeesService = new EmployeesService();
+    APIService.processingOnAPIOfDataModels({ req, res, method: employeesService.get({ empID: +req.params.id }), dataError: null });
+});
+
 router.post('/employees', async (req: express.Request, res: express.Response) => {
     if (!req.body) res.status(400).json({ data: null, message: 'not been entered data to form'});
 
