0dc00fb739c03abbc840f257e8212be53fedd1c1
sergiy-ta <serhiy819@gmail.com>
2021-03-01 22:31:11 +0200

    added method post for api employees

---
+++ server/src/service/employees/employees.service.ts
@@ -0,0 +1,15 @@
+import EmployeesDatabase from "../../database/employees/employees.database";
+import Create from "../../interface/class/create";
+import Employees from "../../interface/object/employees";
+
+export default class EmployeesService implements Create<Employees> {
+    private readonly employeesDatabase: EmployeesDatabase;
+
+    constructor() {
+        this.employeesDatabase = new EmployeesDatabase();
+    }
+
+    public create(info: { empName: string, empActive: boolean, emp_dpID: number }): Promise<Employees> {
+        return this.employeesDatabase.create(info);
+    }
+}
