d078c5cafeabecc23dc51074d6b7c76f6cc54895
sergiy-ta <serhiy819@gmail.com>
2021-03-04 19:04:01 +0200

    added filter to page of employees

--- client/src/app/employees/components/employees-list/employees-list.component.ts
+++ client/src/app/employees/components/employees-list/employees-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Employees } from '../../interface/employees';
 
 @Component({
@@ -6,12 +6,26 @@ import { Employees } from '../../interface/employees';
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.scss']
 })
-export class EmployeesListComponent implements OnInit {
+export class EmployeesListComponent implements OnInit, OnChanges {
   @Input() employeesList: Employees[] = [];
+  list: Employees[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    this.list = this.employeesList;
+  }
+
+  filter(info: { text: string }): void {
+    this.list = [];
+    this.employeesList.forEach((element, index) => {
+      if (element.empName.includes(info.text)) {
+        this.list.push(element);
+      }
+    });
   }
 
 }
