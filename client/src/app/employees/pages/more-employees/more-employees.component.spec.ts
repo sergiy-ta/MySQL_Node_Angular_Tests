6f05f6f39dcb595e4897e75158e28e293fe9df1c
sergiy-ta <serhiy819@gmail.com>
2021-03-04 22:00:18 +0200

    added url from employees table to page with more info

--- client/src/app/employees/pages/more-employees/more-employees.component.spec.ts
+++ client/src/app/employees/pages/more-employees/more-employees.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MoreEmployeesComponent } from './more-employees.component';
 
@@ -8,6 +10,7 @@ describe('MoreEmployeesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ RouterTestingModule, HttpClientModule ],
       declarations: [ MoreEmployeesComponent ]
     })
     .compileComponents();
