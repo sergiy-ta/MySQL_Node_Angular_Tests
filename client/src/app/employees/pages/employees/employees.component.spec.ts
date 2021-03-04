eff0f63877c5bc6e8cc37bf57b5b1d20eecfbe31
sergiy-ta <serhiy819@gmail.com>
2021-03-04 14:48:56 +0200

    fixed error with test in components of employees

--- client/src/app/employees/pages/employees/employees.component.spec.ts
+++ client/src/app/employees/pages/employees/employees.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { EmployeesComponent } from './employees.component';
 
@@ -8,6 +9,7 @@ describe('EmployeesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
       declarations: [ EmployeesComponent ]
     })
     .compileComponents();
