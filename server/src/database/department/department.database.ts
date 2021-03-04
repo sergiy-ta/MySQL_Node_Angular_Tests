8e7425379a01fe66e8eb99f901cdffb2adaea84c
sergiy-ta <serhiy819@gmail.com>
2021-03-04 19:24:35 +0200

    added api for update of department

--- server/src/database/department/department.database.ts
+++ server/src/database/department/department.database.ts
@@ -1,10 +1,11 @@
 import Create from "../../interface/class/create";
 import Get from "../../interface/class/get";
 import GetList from "../../interface/class/getList";
+import Update from "../../interface/class/update";
 import Department from "../../interface/object/department";
 import Database from "../database";
 
-export default class DepartmentDatabase extends Database<Department> implements Create<Department>, Get<Department>, GetList<Department> {
+export default class DepartmentDatabase extends Database<Department> implements Create<Department>, Get<Department>, GetList<Department>, Update {
 
     constructor() {
         super();
@@ -25,5 +26,10 @@ export default class DepartmentDatabase extends Database<Department> implements 
     public getList(): Promise<Department[]> {
         const query = "SELECT * FROM tblDepartments;";
         return super.getListInDatabase({ query });
+    }
+
+    public update(info: Department): Promise<boolean> {
+        const query: string = "UPDATE tblDepartments SET dpName = ? WHERE dpID = ?";
+        return super.updateInDatabase({ query, data: [info.dpName, info.dpID]});
     }
 }
